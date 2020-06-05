d2c35d5d5abfa67a02b0a40bf375aac57f28e8e4
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-06-05 21:31:21 +0700

    slider image jquery

--- galeri_foto_jquery/script.js
+++ galeri_foto_jquery/script.js
@@ -1,4 +1,23 @@
 // memastikan file sudah diload dengan sempurna
 $(document).ready(function(){
 
+	var index = 0,
+		items = $('.container div'),
+		// get total div pada container
+		total = items.length;
+
+	// fungsi slide
+	function slide() {
+		var item = $('.container div').eq(index);
+		items.hide();
+		item.css('display', 'block');
+	}
+
+	// setInterval akan menjalankan fungsi sesuai waktu yang disetting
+	var autoslide = setInterval(function() {
+		index += 1;
+		if (index > total - 1) index = 0;
+		slide();
+	}, 1500);
+
 });
